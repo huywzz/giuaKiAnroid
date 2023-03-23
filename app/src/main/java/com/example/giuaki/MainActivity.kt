1e27b81d7c3690de2c64f78f0e839479099bffb7
HuyHue <dqh1005@gmail.com>
2023-03-23 09:10:43 +0700

    final2

--- app/src/main/java/com/example/giuaki/MainActivity.kt
+++ app/src/main/java/com/example/giuaki/MainActivity.kt
@@ -8,11 +8,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.giuaki.databinding.ActivityMainBinding
+import java.util.regex.Pattern
 
 private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     lateinit var db: SQLiteDatabase
     lateinit var rs: Cursor
+    val EMAIL_ADDRESS_PATTERN = Pattern.compile(
+        "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                "\\@" +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                "(" +
+                "\\." +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                ")+"
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
@@ -21,7 +31,11 @@ class MainActivity : AppCompatActivity() {
         db=helper.readableDatabase
         rs=db.rawQuery("Select * from Users Limit 20",null)
         binding.btnInsert.setOnClickListener {
-          if(binding.edtPassword.text.toString().compareTo(binding.edtPassword2.text.toString())==0 ){
+          if(binding.edtPassword.text.toString().compareTo(binding.edtPassword2.text.toString())==0
+              && isValidString(binding.edtMail.text.toString() )
+              && binding.edtNameUser.text.toString().trim().isNotEmpty()
+              && binding.edtPassword.text.toString().trim().isNotEmpty()
+          ){
               var cv=ContentValues()
               cv.put("email", binding.edtMail.text.toString())
               cv.put("userName", binding.edtNameUser.text.toString())
@@ -37,5 +51,8 @@ class MainActivity : AppCompatActivity() {
               Toast.makeText(this,"Dang ki that bai",Toast.LENGTH_SHORT).show()
           }
         }
+    }
+    fun isValidString(str: String): Boolean{
+        return EMAIL_ADDRESS_PATTERN.matcher(str).matches()
     }
 }
